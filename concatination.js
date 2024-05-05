3ca5ae68e26b431ce0f951b97db5096d8de33fdf
zerolight59 <manusankaru10@gmail.com>
2024-05-05 23:53:29 +0530

    use of + symboles

---
+++ concatination.js
@@ -0,0 +1,31 @@
+var a=5;
+var b="i am ";
+console.log(a);
+console.log(b);
+
+a=a+6;
+console.log(a);
+
+b=b+"Iron Man";
+console.log(b);
+
+var c=a+b;
+console.log(c);
+
+var d="5";
+d=d+5;
+console.log(d);
+
+
+/*
+
+OUTPUTS
+
+5
+VM62:4 i am 
+VM62:7 11
+VM62:10 i am Iron Man
+VM62:13 11i am Iron Man
+VM62:17 55
+
+*/
