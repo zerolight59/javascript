c913f07e996c216736aa49096d8414e2b06efa3a
zerolight59 <manusankaru10@gmail.com>
2024-06-11 00:17:58 +0530

    added index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+
+
+let fullname ="manu Sanker";
+let age=22;
+let isStudent=true
+
+document.getElementById("p1").textContent = `your fulll name is ${fullname}`;
+document.getElementById("p2").textContent = age;
+document.getElementById("p3").textContent = isStudent;
