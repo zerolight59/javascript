f0703930782835c4cc6b94a217ab00ed908a0056
zerolight59 <manusankaru10@gmail.com>
2024-06-12 00:51:05 +0530

    completed program

--- counter/index.js
+++ counter/index.js
@@ -1,1 +1,21 @@
-const dec = document.getElementById("decBtn")
+// counter prograam
+
+const dec = document.getElementById("decBtn");
+const reset=document.getElementById("resetBtn");
+const inc=document.getElementById("incBtn");
+const countLabel=document.getElementById("countlabel");
+
+let count=0;
+
+inc.onclick=function() {
+    count++;
+    countLabel.textContent =count;
+}
+dec.onclick=function() {
+    count--;
+    countLabel.textContent =count;
+}
+reset.onclick=function() {
+    count=0;
+    countLabel.textContent =count;
+}
