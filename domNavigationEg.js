bfd158d146fa3f0ff259aecc3f9c4bd44af53583
zerolight59 <manusankaru10@gmail.com>
2024-07-01 01:50:33 +0530

    dom navigation

--- domNavigationEg.js
+++ domNavigationEg.js
@@ -13,19 +13,43 @@
 
 
 
-const elements= document.getElementById("fruits");
-const LChild=elements.lastElementChild;
-LChild.style.backgroundColor="yellow";
+// const elements= document.getElementById("fruits");
+// const LChild=elements.lastElementChild;
+// LChild.style.backgroundColor="yellow";
 
 
 
-const ulElements=document.querySelectorAll("ul");
-ulElements.forEach(ulElement =>{
-    const laChild=ulElement.lastElementChild;
-    laChild.style.backgroundColor="lightgreen";
+// const ulElements=document.querySelectorAll("ul");
+// ulElements.forEach(ulElement =>{
+//     const laChild=ulElement.lastElementChild;
+//     laChild.style.backgroundColor="lightgreen";
+// })
+
+
+// const selements= document.getElementById("vegetables");
+// const nectChild=selements.nextElementSibling;
+// nectChild.style.backgroundColor="lightgreen";
+
+
+
+// const pelements= document.getElementById("vegetables");
+// const preChild=selements.previousElementSibling;
+// preChild.style.backgroundColor="orange";
+
+
+
+
+//  const parElements = document.getElementById("ice");
+//  const ParentEle= parElements.parentElement;
+//  ParentEle.style.backgroundColor="skyblue"
+
+
+
+
+const cElements = document.getElementById("fruits");
+const childrenEle= cElements.children;
+Array.from(childrenEle).forEach(child=>{
+    child.style.backgroundColor="orange"
 })
-
-
-const selements= document.getElementById("apple");
-const nectChild=selements.nextElementSibling;
-nectChild.style.backgroundColor="lightred";
+childrenEle[1].style.backgroundColor="black";
+childrenEle[1].style.color="blue"
